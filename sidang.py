d5a678c1f3a4161a01d84fede392d96b69adfabd
Imam Syafiih <43528632+imamsyafiihreal@users.noreply.github.com>
2024-07-10 22:43:23 +0700

    Update sidang.py

--- sidang.py
+++ sidang.py
@@ -18,12 +18,12 @@ from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from tqdm import tqdm
 
 # Tentukan lokasi direktori nltk_data lokal
-nltk_data_path = os.path.join(os.path.dirname(__file__), 'nltk_data')
+nltk_data_path = https://github.com/imamsyafiihreal/sidang_ready/tree/main/nltk/corpora
 nltk.data.path.append(nltk_data_path)
 
 # Sekarang unduh data yang diperlukan (ini akan mengambil dari direktori lokal)
-nltk.download('stopwords', download_dir=nltk_data_path)
-nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('stopwords', download_dir=https://github.com/imamsyafiihreal/sidang_ready/tree/main/nltk/corpora/punkt)
+nltk.download('punkt', download_dir=https://github.com/imamsyafiihreal/sidang_ready/tree/main/nltk/corpora/stopwords)
 
 # Download resource NLTK yang dibutuhkan
 nltk.download('stopwords')
